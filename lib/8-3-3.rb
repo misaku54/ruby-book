1fe66858d7d12eae7385e4e6bee044c7d76ccc51
misaku54 <eisei0504@outlook.jp>
2023-03-19 03:41:42 +0900

    lib/8-3-3.rb

---
+++ lib/8-3-3.rb
@@ -0,0 +1,17 @@
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class Product
+  # Loggableモジュールのメソッドを特異メソッド(classメソッド)として追加する。
+  extend Loggable
+
+  def self.create_products(names)
+    self.log 'create_products is called'
+  end
+end
+
+Product.create_products([])
+Product.log('aaa')
