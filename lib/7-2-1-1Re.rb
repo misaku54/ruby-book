c499309339152f32b7d6b0930c10d7f5eb4ca73d
misaku54 <eisei0504@outlook.jp>
2023-06-16 23:50:52 +0900

    [update]

--- lib/7-2-1-1Re.rb
+++ lib/7-2-1-1Re.rb
@@ -7,6 +7,10 @@ class User
     @last_name  = last_name
     @age        = age
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
 
 # ユーザーのデータを作成する
@@ -14,10 +18,6 @@ users = []
 users << User.new('Alice', 'Ruby', 20)
 users << User.new('Bob', 'Python', 30)
 
-def full_name(user)
-  "#{user.first_name} #{user.last_name}"
-end
-
 users.each do |user| 
-  puts "氏名: #{full_name(user)}、年齢：#{user.age}"
+  puts "氏名: #{user.full_name}、年齢：#{user.age}"
 end
