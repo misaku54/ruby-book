25657fd0cde4828ec89c8d8b41c1a575ec5df62d
misaku54 <eisei0504@outlook.jp>
2023-08-27 08:48:20 +0900

    [update]

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,4 +1,14 @@
 # putsは自動的に改行が末尾に付与されるため。
 print 'Text?:'
 text = gets.chomp
-puts text
+print 'Pattern?:'
+pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+
+if matches.size > 0
+  puts "Matched: #{matches.join(',')}"
+else
+  puts 'Nothing matched.'
+end
