8d2ff4c6f460f39963a53209e064e979b81f3e4f
misaku54 <eisei0504@outlook.jp>
2023-08-27 08:50:52 +0900

    [update]

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,12 +1,18 @@
 # putsは自動的に改行が末尾に付与されるため。
 print 'Text?:'
 text = gets.chomp
-print 'Pattern?:'
-pattern = gets.chomp
 
-regexp = Regexp.new(pattern)
+# 例外処理を組み込んで再入力可能にする。
+begin
+  print 'Pattern?:'
+  pattern = gets.chomp
+  regexp  = Regexp.new(pattern)
+rescue
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
+
 matches = text.scan(regexp)
-
 if matches.size > 0
   puts "Matched: #{matches.join(',')}"
 else
